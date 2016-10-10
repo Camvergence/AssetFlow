1dced12bad34af2f57afb6efbc8b7fef17b3cbcc
Matt Klosterman <matt@klosterman.net>
2016-10-10 16:54:24 -0500

    Add .travis.yml

---
+++ AssetFlow/Cameras/Olympus/OlympusEM1.swift
@@ -0,0 +1,9 @@
+//
+//  OlympusEM1.swift
+//  AssetFlow
+//
+//  Created by Matt Klosterman on 10/10/16.
+//  Copyright © 2016 Infofission LLC. All rights reserved.
+//
+
+import Foundation
---
+++ AssetFlow/Cameras/Olympus/OlympusEP5.swift
@@ -0,0 +1,9 @@
+//
+//  OlympusEP5.swift
+//  AssetFlow
+//
+//  Created by Matt Klosterman on 10/10/16.
+//  Copyright © 2016 Infofission LLC. All rights reserved.
+//
+
+import Foundation
