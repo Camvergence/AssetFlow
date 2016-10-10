9cf90f709bbac31507784c46ea624d0565e2495e
Matt Klosterman <matt@klosterman.net>
2016-10-10 12:51:37 -0500

    Add additional cameras

---
+++ AssetFlow/Cameras/Nikon/NikonD750.swift
@@ -0,0 +1,17 @@
+//
+//  Asset Flow, https://github.com/Camvergence/AssetFlow
+//
+//  Copyright (c) 2016-2016 Matt Klosterman and contributors. All rights reserved.
+//
+
+import Foundation
+
+extension Cameras.Manufacturers.Nikon {
+    public struct D750: CameraModel {
+        public init() {}
+        public let name = "Nikon D750"
+        public let manufacturerType: CameraManufacturer.Type = Cameras.Manufacturers.Nikon.self
+    }
+}
+
+public typealias NikonD750 = Cameras.Manufacturers.Nikon
