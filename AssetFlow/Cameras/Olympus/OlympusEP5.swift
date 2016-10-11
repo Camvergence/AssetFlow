bca57b791450a443b279cd74d745149c1b5cb505
Matt Klosterman <matt@klosterman.net>
2016-10-11 07:54:34 -0500

    Add Olympus E-P5 and Olympus E-M1

--- AssetFlow/Cameras/Olympus/OlympusEP5.swift
+++ AssetFlow/Cameras/Olympus/OlympusEP5.swift
@@ -1,9 +1,17 @@
 //
-//  OlympusEP5.swift
-//  AssetFlow
+//  Asset Flow, https://github.com/Camvergence/AssetFlow
 //
-//  Created by Matt Klosterman on 10/10/16.
-//  Copyright © 2016 Infofission LLC. All rights reserved.
+//  Copyright (c) 2016-2016 Matt Klosterman and contributors. All rights reserved.
 //
 
 import Foundation
+
+extension Cameras.Manufacturers.Olympus {
+    public struct EP5: CameraModel {
+        public init() {}
+        public let name = "Olympus E-P5"
+        public let manufacturerType: CameraManufacturer.Type = Cameras.Manufacturers.Olympus.self
+    }
+}
+
+public typealias OlympusEP5 = Cameras.Manufacturers.Olympus.EP5
