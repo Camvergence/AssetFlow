946146e5747a51db7f552f0a8ee6f21e77d44233
Matt Klosterman <matt@klosterman.net>
2016-10-11 20:18:28 -0500

    Add AssetFlowTV framework

---
+++ AssetFlowTV/AssetFlowTV.h
@@ -0,0 +1,19 @@
+//
+//  AssetFlowTV.h
+//  AssetFlowTV
+//
+//  Created by Matt Klosterman on 10/11/16.
+//  Copyright © 2016 Infofission LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssetFlowTV.
+FOUNDATION_EXPORT double AssetFlowTVVersionNumber;
+
+//! Project version string for AssetFlowTV.
+FOUNDATION_EXPORT const unsigned char AssetFlowTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetFlowTV/PublicHeader.h>
+
+
