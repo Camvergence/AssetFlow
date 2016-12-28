73f6c9d43cd846e459619ca1fddde5273e5449b8
Matt Klosterman <matt@klosterman.net>
2016-12-28 14:28:29 -0600

    Minor clean up

--- PhotosPlus/PhotosPlus.h
+++ PhotosPlus/PhotosPlus.h
@@ -1,9 +1,7 @@
 //
-//  PhotosPlus.h
-//  PhotosPlus
+//  Photos Plus, https://github.com/LibraryLoupe/PhotosPlus
 //
-//  Created by Matt Klosterman on 10/4/16.
-//  Copyright © 2016 Infofission LLC. All rights reserved.
+//  Copyright (c) 2016-2016 Matt Klosterman and contributors. All rights reserved.
 //
 
 @import Foundation;
@@ -13,7 +11,5 @@ FOUNDATION_EXPORT double PhotosPlusVersionNumber;
 
 //! Project version string for PhotosPlus.
 FOUNDATION_EXPORT const unsigned char PhotosPlusVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PhotosPlus/PublicHeader.h>
 
 
