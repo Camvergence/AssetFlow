42b5222a13a6d4a3b6f7c0b60557ee01390acca0
Matt Klosterman <matt@klosterman.net>
2017-01-01 13:43:44 -0600

    Additional convenience extension

---
+++ PhotosPlus/PhotosPlus/PHAssetResourceType_CustomStringConvertible.swift
@@ -0,0 +1,38 @@
+//
+//
+//  Photos Plus, https://github.com/LibraryLoupe/PhotosPlus
+//
+//  Copyright (c) 2016-2017 Matt Klosterman and contributors. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+extension PHAssetResourceType : CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .adjustmentBasePairedVideo:
+            return "Adjustment Base Paired Video"
+        case .adjustmentBasePhoto:
+            return "Adjustment Base Photo"
+        case .adjustmentData:
+            return "Adjustment Data"
+        case .alternatePhoto:
+            return "Alternate Photo"
+        case .audio:
+            return "Audio"
+        case .fullSizePairedVideo:
+            return "Full Size Paired Video"
+        case .fullSizePhoto:
+            return "Full Size Photo"
+        case .fullSizeVideo:
+            return "Full Size Video"
+        case .photo:
+            return "Photo"
+        case .video:
+            return "Video"
+        default:
+            return String(format: "Unknown (%d)", self.rawValue)
+        }
+    }
+}
